b030c3deeacc08e0e30a1bc8812adbd1a01c8404
Thiago Barros <thiago-svl@hotmail.com>
2025-10-16 00:42:21 -0300

    ajustes

--- supabase/functions/adminCreateUser/index.ts
+++ supabase/functions/adminCreateUser/index.ts
@@ -3,14 +3,15 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.57.0";
 import { corsHeaders } from "../../_shared/cors.ts";
 
 serve(async (req) => {
+
   if (req.method === 'OPTIONS') {
     return new Response(null, {
       headers: corsHeaders
     });
   }
   try {
-    const supabaseUrl = Deno.env.get('SUPABASE_URL');
-    const serviceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
+  const supabaseUrl = Deno.env.get('SUPABASE_URL');
+  const serviceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
 
     if (!supabaseUrl || !serviceRoleKey) {
       throw new Error("Supabase environment variables are not set.");
