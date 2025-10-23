596431f03e1466522b30ef087a8094ecac110c77
Thiago Barros <thiago-svl@hotmail.com>
2025-10-23 12:23:40 -0300

    [FORCE]

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -199,7 +199,7 @@ export default function Login() {
       />
 
       <Tabs value={tab} onValueChange={setTab} className="w-full max-w-md mb-4">
-        <TabsList className="grid w-full grid-cols-2">
+        <TabsList className="grid w-full grid-cols-2 border">
           <TabsTrigger
             value="motorista"
             className="data-[state=inactive]:text-gray-600 
