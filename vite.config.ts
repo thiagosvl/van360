20c883ac5eef72544355d9bfa0b7a847a8b57396
Thiago Barros <seu-email@exemplo.com>
2026-03-24 18:20:09 -0300

    [FORCE]

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
-import Prerender from "vite-plugin-prerender";
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -20,19 +19,6 @@ export default defineConfig(({ mode }) => ({
       filename: 'dist/stats.html',
       gzipSize: true,
       brotliSize: true,
-    }),
-    // Habilitar Prerendering apenas no build de produção
-    mode === 'production' && Prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/', '/login', '/cadastro'],
-      postProcess(renderedRoute) {
-        // Otimização básica no HTML gerado
-        renderedRoute.html = renderedRoute.html
-          .replace(/<script (.*?) src="\/src\/main.tsx"><\/script>/g, '<script $1 src="/assets/index-*.js"></script>')
-          .replace(/(href|src)="\//g, '$1="./'); // Ajusta caminhos para serem relativos se necessário
-        
-        return renderedRoute;
-      },
     }),
   ].filter(Boolean),
   resolve: {
