977374e46e57335d5a046c5c34573bac04c75886
Thiago Barros <ext.thiago.abilio@mills.com.br>
2026-06-15 15:05:50 -0300

    [FORCE]

--- src/constants/paymentMethods.ts
+++
@@ -1,6 +0,0 @@
-import { CheckoutPaymentMethod } from "@/types/enums";
-
-export const PAYMENT_METHOD_LABELS: Record<CheckoutPaymentMethod, string> = {
-  [CheckoutPaymentMethod.PIX]: "Pix",
-  [CheckoutPaymentMethod.CREDIT_CARD]: "Cartão",
-};
